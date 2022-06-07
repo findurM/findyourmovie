390c2bb4ce0ce9691670dd0de0aee0a7130d832f
김희웅 <hank29206880@gmail.com>
2022-06-07 11:46:42 +0900

    landingpage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
     const [MainMovieImage, setMainMovieImage] = useState(null)
 
     useEffect(() => {
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
