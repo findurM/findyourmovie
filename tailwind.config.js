7e9b9d279483047f14b4a0bac9039d07d2bafbdb
Sebin-Myung <wrqewrq213@naver.com>
2022-06-07 15:33:20 +0900

    테마 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    mode: 'jit',
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'class',
-    theme: {
-        screens: {
-            sm: '640px',
-            md: '768px',
-            lg: '1025px',
-            xl: '1280px',
-            xl2: '1360px',
-        },
-        extend: {},
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1025px',
+      xl: '1280px',
+      xl2: '1360px',
     },
-    plugins: [require('daisyui')],
-    daisyui: {
-        styled: true,
-        themes: ['emerald', 'dark', 'forest', 'synthwave'],
-        base: true,
-        utils: true,
-        logs: true,
-        rtl: false
-    },
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['bumblebee'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "luxury"
+  },
 };
