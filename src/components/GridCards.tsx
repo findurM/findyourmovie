334f1d5d596def1a5840eab864107f7d880ce480
Sebin-Myung <wrqewrq213@naver.com>
2022-06-09 15:29:44 +0900

    GridCards 가로 세로 비율 설정

--- src/components/GridCards.tsx
+++ src/components/GridCards.tsx
@@ -8,7 +8,9 @@ interface Props {
 const GridCards = ({image, movieName}:Props) => {
   return(
     <div className="card card-compact bg-base-100 shadow-xl w-full max-w-[305px]">
-      <figure className='w-full h-full'><img src={image} alt={movieName} className="w-full h-full object-cover hover:scale-125 duration-100" /></figure>
+      <figure className='w-full h-full'>
+        <img src={image} alt={movieName} style={{aspectRatio: "1 / 1.5"}} className="w-full h-full object-cover hover:scale-125 duration-100" />
+      </figure>
     </div>
   )
 }
