c2bbac8f6902cabe7db71153f26933e2fae6bf42
Sebin-Myung <wrqewrq213@naver.com>
2022-06-10 23:00:15 +0900

    최근 검색한 movieId firestore에 저장하기

--- src/pages/DetailedPages.tsx
+++ src/pages/DetailedPages.tsx
@@ -1,3 +1,5 @@
+import { getAuth } from "firebase/auth";
+import { arrayRemove, arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db, CurrentUserInfo } from "../Application";
@@ -8,18 +10,39 @@ export interface DetailedPages {}
 const DetailedPages: React.FC<DetailedPages> = () => {
   const [movieFullDetails, setMovieFullDetails] = useState([])
   const movieId = useParams().id
- 
+  const auth = getAuth();
 
-useEffect(() => {
-  async function fetchData(){
-    const movieDetailApi = `${API_URL}/movie/${movieId}?api_key=${API_KEY}`
-    const res = await fetch(movieDetailApi)
-    const results = await res.json()
-    setMovieFullDetails(results)
-    return results
+  const recordRef = doc(db, "users", auth.currentUser?.uid, "recentRecords", "movies");
+  const getRecentRecords = async () => {
+    const recordSnap = await getDoc(recordRef);
+    const result = recordSnap.data();
+    if(result === undefined) {
+      return false;
+    } else if((result.movieArray as Number[]).length >= 20) {
+      const oldestRecord = result.movieArray[0];
+      updateDoc(recordRef, {movieArray: arrayRemove(oldestRecord)});
+    }
+    return true;
   }
-  fetchData()
-},[])
+  
+  useEffect(() => {
+    getRecentRecords()
+      .then((isExist)  => {
+        if (!isExist) {
+          setDoc(recordRef, {movieArray: [Number(movieId)]});
+        } else {
+          updateDoc(recordRef, {movieArray: arrayUnion(Number(movieId))});
+        }
+      })
+    async function fetchData(){
+      const movieDetailApi = `${API_URL}/movie/${movieId}?api_key=${API_KEY}`
+      const res = await fetch(movieDetailApi)
+      const results = await res.json()
+      setMovieFullDetails(results)
+      return results
+    }
+    fetchData()
+  },[])
 
 
  
