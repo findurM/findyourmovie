0407668da722d9daf2c5067635e6300a625c6ecc
Sebin-Myung <wrqewrq213@naver.com>
2022-06-13 21:13:05 +0900

    성별, 나이 입력하지 않았을 때 메인페이지에 입력 모달 띄우기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,10 @@ import { Link } from "react-router-dom";
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Carousel from "../components/Carousel";
 import tw from "tailwind-styled-components/dist/tailwind";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../Application";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { InputBox } from "./Register";
 
 const Ranking = tw.div`
 absolute
@@ -26,10 +30,25 @@ export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
 
+  const auth = getAuth();
   const [Movies, setMovies] = useState([])
   const [hasMore, setHasMore] = useState(true)
   const [page, setPage] = useState(2)
   const [bestMovies, setBestMovies] = useState([])
+  const [age, setAge] = useState(20)
+  const [selectedRadio, setSelectedRadio] = useState('여')
+
+  const ageRef = useRef<HTMLInputElement>()
+
+  const handleRange = () => {
+    setAge(Number(ageRef.current.value))
+  }
+
+  const isRadioSelected = (value:string) : boolean => selectedRadio === value
+
+  const handleRadio = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSelectedRadio(e.currentTarget.value)
+  }
 
   const movieCarouselRef = useRef<HTMLDivElement>()
   const dramaCarouselRef = useRef<HTMLDivElement>()
@@ -44,12 +63,32 @@ const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
       setBestMovies(bestIds)
     })
   },[])
+  
+  useEffect(() => {
+    const AuthCheck = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const getUserInfo = async () => {
+          const userRef = doc(db, "users", user.uid);
+          const userSnap = await getDoc(userRef);
+          return userSnap.data();
+        }
+        getUserInfo()
+        .then((data) => {
+          if(data.age === undefined || data.sex === undefined) {
+            (document.querySelector("label.modal-button") as HTMLLabelElement).click();
+          }
+        });
+      }
+    });
 
- const fetchMovies = async () => {
-   const res = await fetch(`${API_URL}movie/top_rated?api_key=${API_KEY}&language=en-US&page=${page}`)
-   const data = await res.json()
-   return data.results
- }
+    return () => AuthCheck();
+  }, [auth]);
+
+  const fetchMovies = async () => {
+    const res = await fetch(`${API_URL}movie/top_rated?api_key=${API_KEY}&language=en-US&page=${page}`)
+    const data = await res.json()
+    return data.results
+  }
 
   const fetchData = async () => {
     const newMovies = await fetchMovies()
@@ -61,9 +100,59 @@ const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
     }
   }
 
+  const updateUserInfo = async () => {
+    await updateDoc(doc(db,"users", auth.currentUser?.uid),
+                          {age: age,
+                          sex: selectedRadio})
+  }
+
   return (
     <>
       <MainImage image={`/assets/FindurM_main_hero.jpg`}/>
+
+      <label htmlFor="my-modal-6" className="btn modal-button" style={{display: "none"}}>open modal</label>
+
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg mb-5">성별과 나이를 입력해주세요!</h3>
+          <InputBox>
+            <label htmlFor="sex">
+              성별
+            </label>
+            <div className='mt-3 w-full flex'>
+              <div className='mr-3 flex justify-between w-10'> 
+                <span>남</span>
+                <input type="radio" name="radio-3" value='남' className="radio radio-secondary" 
+                checked={isRadioSelected('남')} 
+                onChange={handleRadio} />
+              </div>
+              <div className='flex justify-between w-10'>
+                <span>여</span>
+                <input type="radio" name="radio-3" value='여' className="radio radio-secondary" 
+                  checked={isRadioSelected('여')} 
+                  onChange={handleRadio} />
+              </div>
+            </div>
+          </InputBox>
+          <InputBox>
+            <label htmlFor="age">
+              나이: {age}세
+            </label>
+            <input 
+            type="range" min="10" max="90" value={age} step="1" className="range range-secondary my-3"
+            id='age'
+            name='age'
+            onChange={handleRange}
+            ref={ageRef}
+            />
+          </InputBox>
+          <div className="modal-action">
+            <label htmlFor="my-modal-6" className="btn" onClick={updateUserInfo}>확인</label>
+          </div>
+        </div>
+      </div>
+
       <section>
           <Carousel category='최신'/>
           <Carousel category='개봉예정'/>
