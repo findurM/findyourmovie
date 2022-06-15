5124a66561094562d5c7254803bfb756b14f94a1
susu <isks2838@gmail.com>
2022-06-15 09:48:19 +0900

    DetailedPages 구조분해할당 및 버튼 활성화

--- src/pages/DetailedPages.tsx
+++ src/pages/DetailedPages.tsx
@@ -4,23 +4,50 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db, CurrentUserInfo } from "../Application";
 import { API_URL,API_KEY,IMAGE_URL } from "../config/config"
+import {BsHeart,BsFillHeartFill} from 'react-icons/bs'
 
 
-export interface DetailedPages {}
+export interface MovieDetailedPages {}
+
+interface MovieFullDetails {
+  adult?: boolean,
+  backdrop_path?: string,
+  belongs_to_collection?: null | object,
+  budget?: number,
+  genres?: [{id: number , name: string}],
+  genre_ids?: [number],
+  homepage?: string | null,
+  id?: number,
+  imdb_id?: string | null,
+  original_language?: string,
+  original_title?: string,
+  overview?: string | null,
+  popularity?: number,
+  poster_path?: string | null,
+  production_companies?: [{id: number, logo_path: string, name: string, origin_country: string}],
+  production_countries?: [{iso_3166_1: string, name: string}],
+  release_date?: string,
+  revenue?: number,
+  runtime?: number | null,
+  spoken_languages?: [{iso_639_1: string, name: string}],
+  status?: string,
+  tagline?: string | null,
+  title?: string,
+  video?: boolean,
+  vote_average?: number,
+  vote_count?: number
+}
 
 
-
-const DetailedPages: React.FC<DetailedPages> = () => {
-  const [movieFullDetails, setMovieFullDetails] = useState([])
+const DetailedPages: React.FC<MovieDetailedPages> = () => {
+  const [movieFullDetails, setMovieFullDetails] = useState<MovieFullDetails>()
   const [actors, setActors] = useState([])
   const [director, setDirector] = useState([])
+  const [similarMovies, setSimilarMovies] = useState([])
+  const [like, setLike] = useState(false)
+  const [moreCredits, setMoreCredits] = useState(false)
   const movieId = useParams().id
 
-  interface Genre {
-    id: number, 
-    name: string,
-  }
-  
   interface ActorInfo {
     adult: boolean
     cast_id: number
@@ -45,15 +72,20 @@ useEffect(() => {
       updateDoc(recordRef, {movieArray: arrayUnion(Number(movieId))});
     }
   })
+},[])
 
+useEffect(() => {
   async function fullDetails(){
     const movieDetailApi = `${API_URL}/movie/${movieId}?api_key=${API_KEY}`
     const res = await fetch(movieDetailApi)
     const results = await res.json()
-    setMovieFullDetails([results])
+    setMovieFullDetails(results)
     return results
   }
+  fullDetails()
+},[])
 
+useEffect(() => {
   async function actorDetails(){
     const actorDetailApi = `${API_URL}/movie/${movieId}/credits?api_key=${API_KEY}`
     const res = await fetch(actorDetailApi)
@@ -63,8 +95,22 @@ useEffect(() => {
     return results
   }
 
-  fullDetails()
+
   actorDetails()
+},[]) 
+
+useEffect(() => {
+  async function similarMovies(){
+    const similarMoviesApi = `${API_URL}/movie/${movieId}/similar?api_key=${API_KEY}`
+    const res = await fetch(similarMoviesApi)
+    const results = await res.json()
+    setSimilarMovies(results.results)
+    
+    return results
+  }
+
+
+  similarMovies()
 },[]) 
 
   const auth = getAuth();
@@ -110,29 +156,64 @@ function maxFiveActors(actors: Array<ActorInfo>): Array<ActorInfo> {
   return result
 }
 
-const movieTitle: string = movieFullDetails[0]?.original_title
-const movieGenres: Array<Genre> = movieFullDetails[0]?.genres
-const moviePoster: string = movieFullDetails[0]?.poster_path
-const movieImage: string = movieFullDetails[0]?.backdrop_path
-const movieRelease: string = movieFullDetails[0]?.release_date
-const movieLanguage: string = movieFullDetails[0]?.original_language
-const movieRuntime: string = movieFullDetails[0]?.runtime
-const movieOverview: string = movieFullDetails[0]?.overview
+function maxSevenMovies(movies: Array<MovieFullDetails>): Array<MovieFullDetails> {
+  const result: Array<MovieFullDetails> = []
+  if(movies.length > 7){
+    for(let i = 0; i < 7; i++){
+      result.push(movies[i])
+    }
+  } else {
+    return movies
+  }
+  return result
+}
+
+let movieTitle 
+let movieGenres 
+let moviePoster 
+let movieImage 
+let movieRelease 
+let movieLanguage 
+let movieRuntime
+let movieOverview 
+
+if(movieFullDetails !== undefined){
+  const {original_title, genres, poster_path, backdrop_path, release_date, spoken_languages, runtime, overview} = movieFullDetails
+
+  movieTitle = original_title
+  movieGenres = genres
+  moviePoster = poster_path
+  movieImage = backdrop_path
+  movieRelease = release_date
+  movieLanguage = spoken_languages[0].name
+  movieRuntime = runtime
+  movieOverview = overview
+
+}
 
 const movieDirector: string = director[0]?.name
-
+  
 const fiveMovieActors: JSX.Element[] = maxFiveActors(actors).map((actor) => <li key={actor.credit_id}><img className="w-20" src={`${IMAGE_URL}w300${actor.profile_path}`} alt='Actor Image'></img><p>{actor.character}역</p> <p>{actor.name}</p></li>)
 const tenMovieActors: JSX.Element[] = maxTenActors(actors).map((actor) => <li key={actor.credit_id}><img className="w-20" src={`${IMAGE_URL}w300${actor.profile_path}`} alt='Actor Image'></img><p>{actor.character}역</p> <p>{actor.name}</p></li>)
 
+const sevenSimilarMovies: JSX.Element[] = maxSevenMovies(similarMovies).map((movie)=> <li key={movie.id} className="mr-10"><img className="w-36" src={movie.poster_path ? `${IMAGE_URL}w300${movie.poster_path}`: null} alt='Similar Movie Image'/><p>{movie.title}</p></li>)
+
+
+let imgUrl = ""
+if(movieImage !== undefined) {
+  imgUrl = `${IMAGE_URL}w500${movieImage}` 
+}
 
 
   return (
     <>
     <section>
-      <img className="w-full" src={movieImage&& `${IMAGE_URL}w500${movieImage}`} alt="hero-image"></img>
+      <div className="opacity-50" style={{backgroundImage: `url(${imgUrl})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', width: '100%', height: '40vw'}}> </div>
+      
     <h2>{movieTitle}</h2>
-    <button>좋아요</button>
+    <button className="btn btn-accent btn-sm rounded-2xl w-28" onClick={()=> like ? setLike(false) : setLike(true)}>{like ? <BsFillHeartFill className="mr-3 text-red-600"></BsFillHeartFill> : <BsHeart className="mr-3"></BsHeart>}좋아요</button>
     </section>
+
     <section>
       
       <img src={moviePoster&& `${IMAGE_URL}w300${moviePoster}`} alt="poster"></img>
@@ -151,16 +232,28 @@ const tenMovieActors: JSX.Element[] = maxTenActors(actors).map((actor) => <li ke
       </ul>
       <ul className='flex flex-row'>
         출연진
-        {fiveMovieActors}
-        <li><button>더보기</button></li>
+        {moreCredits ? tenMovieActors : fiveMovieActors}
+        <li><button className='btn btn-primary btn-sm' onClick={()=>moreCredits ? setMoreCredits(false) :setMoreCredits(true)}>{moreCredits ? `접기` : `더보기`}</button></li>
       </ul>
       
     
     </section>
+
+    <section>
     <div>
       <h3>줄거리</h3>
       <p>{movieOverview&&movieOverview}</p>
     </div>
+    <h3>비슷한 영화</h3>
+    <ul className='flex flex-row'>
+      {sevenSimilarMovies}
+    </ul>
+    <div>
+
+    </div>
+    </section>
+    
+
     <section>
       <h3>감상평</h3>
     </section>
