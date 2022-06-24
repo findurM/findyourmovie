96d0b553edc5c135d877b4ee5cc72dcf0b2291b4
Sebin-Myung <wrqewrq213@naver.com>
2022-06-24 22:51:22 +0900

    로그인 시 userInfo 새로 받아오도록 수정

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -3,35 +3,38 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { setUserInfo } from '../features/userSlice';
 import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../app/store';
+import { fetchUserInfo } from '../features/fetchUserInfoSlice';
 
 export interface IAuthRouteProps {}
 
 const AuthRoute: React.FunctionComponent<IAuthRouteProps> = (props) => {
-    //props에 디폴트로 들어있는 property children 자식 컴포넌트 전달
-    const { children } = props;
-    const auth = getAuth();
-    const dispatch = useDispatch()
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
+  //props에 디폴트로 들어있는 property children 자식 컴포넌트 전달
+  const { children } = props;
+  const auth = getAuth();
+  const dispatch = useDispatch<AppDispatch>()
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const AuthCheck = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setLoading(false);
-                localStorage.setItem('user',JSON.stringify(user))
-                dispatch(setUserInfo({email: user.email}))
-            } else {
-                console.log('unauthorized');
-                navigate('/login');
-            }
-        });
+  useEffect(() => {
+    const AuthCheck = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoading(false);
+        localStorage.setItem('user',JSON.stringify(user))
+        dispatch(setUserInfo({email: user.email}))
+        dispatch(fetchUserInfo());
+      } else {
+        console.log('unauthorized');
+        navigate('/login');
+      }
+    });
 
-        return () => AuthCheck();
-    }, [auth]);
+    return () => AuthCheck();
+  }, [auth]);
 
-    if (loading) return <p>loading ...</p>;
+  if (loading) return <p>loading ...</p>;
 
-    return <div style={{height:'100vh'}}>{children}</div>;
+  return <div style={{height:'100vh'}}>{children}</div>;
 };
 
 export default AuthRoute;
